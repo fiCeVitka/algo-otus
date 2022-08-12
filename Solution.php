0dbb9fb6ddc0960d36f908928a42943be18dce32
viktor <gmac.net.ru@gmail.com>
2022-08-13 00:03:24 +0300

    Решение задачи с счастливыми числами

--- Solution.php
+++ Solution.php
@@ -2,8 +2,28 @@
 
 class Solution
 {
-    public function solve()
+    public function solve(int $n)
     {
-        return;
+        $sum = [];
+
+        for ($i = 1; $i <= $n; $i++) {
+            $sum[$i] ??= [];
+
+            for ($j = 0; $j <= 9*$i; $j++) {
+                if ($i === 1) {
+                    $sum[$i][$j] = 1;
+                    continue;
+                }
+
+                for ($k = 0; $k <= 9; $k++) {
+                    @
+                    $sum[$i][$j] += ($sum[$i - 1][$j - $k] ?? 0);
+                }
+
+
+            }
+        }
+
+        return array_reduce($sum[$n], fn ($res, $value) => $res + $value * $value, 0);
     }
 }
